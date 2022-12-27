d3fc424fa1508cf879f7aa002389d11c1633d1f0
HellOr0BEN <rabin.timalsina@riddhasoft.com>
2022-12-27 15:42:58 +0545

    update facility api implement

--- src/http/endpoints/endpoints.ts
+++ src/http/endpoints/endpoints.ts
@@ -13,7 +13,8 @@ export const saveFacilities = BaseUrl + "facility/create";
 export const getFacilities = BaseUrl + "facility/get-all";
 export const deleteFacilities = (id: string) =>
   BaseUrl + `facility/delete?id=${id}`;
-export const editFacilities = BaseUrl + "";
+export const editFacilities = (id: string) =>
+BaseUrl + `facility/update?id=${id}`;
 
 export const visitorsCheckInCheckOut = BaseUrl + "";
 export const visitorsFacilityCheckInCheckOut = BaseUrl + "";
